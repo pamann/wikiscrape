41b9416311da1a6c650b1a50235225c3f39101fa
Pam <beardsell.pamela@gmail.com>
2020-11-11 18:15:32 -0500

    slow but working, added data restruct and saving as json files

--- bfs.py
+++ bfs.py
@@ -1,10 +1,12 @@
 from pywikiapi import wikipedia as pywiki
 import wikipedia
 import time
+import json
+from collections import Counter
 
-root_term = "Homer Simpson"
 nodes = []
 links = []
+root_term = "Halloween"
 
 def fetch_links(root_term):
     site = pywiki('en')
@@ -12,23 +14,48 @@ def fetch_links(root_term):
     root = wikipedia.page(search_r[0])
     root_id = root.pageid
     count = 0
-    for page in site.query(generator="links", titles=root_term, format="json", prop=["links", "pageprops"], gpllimit="max"):
-        count += 1
-        if count == 2:
-            break
+    for page in site.query(format="json", pllimit="max", titles=root_term, prop="links", redirects=True, plnamespace=0):
+        page = page.pages[0]
+        gen = []
         sub_nodes = []
-        link_data = list({ "meta": {"id": (page.pageid if "pageid" in page else "null") , "name": page.title}} for page in page.pages)
-        res = {"meta": { "name": (page.title if "title" in page else "null"), "id": (page.pageid if "pageid" in page else "null") }, "links": link_data}            
-        print(res)
+        count += len(page.links)
+        aggregate_links(root_term, page.links)
+        nodes.append(root_term)
+        for link in page.links:
+            for page_2 in site.query(format="json", pllimit="max", titles=link.title, prop="links", redirects=True, plnamespace=0):
+                if "links" in page_2.pages[0]:
+                    scnd_l = page_2.pages[0].links
+                    aggregate_links(link.title, scnd_l)
+                    count += len(scnd_l)
+                    nodes.append(link.title)
+                    nodes.extend(list(l2.title for l2 in scnd_l))
+        return count
 
-def aggregate_links(links, node):
-    links.append({node: links})
+def aggregate_links(node, res):
+    obj = ({
+        "source": node,
+        "target": link_dest.title
+    } for link_dest in res)
+    links.extend(obj)
+
+def count_nodes():
+    counts = Counter(nodes)
+    filtered_nodes = {k: c for k, c in counts.items() if c >= 20}
+    filtered_counts = Counter(filtered_nodes)
+    return (sorted(filtered_counts.items(), key=lambda x: x[1], reverse=True), filtered_nodes)
 
 start_time = time.time()
-fetch_links("Halloween")
+print(fetch_links(root_term))
 timer = time.time() - start_time
 
-print(nodes)
-print(len(nodes))
-print(f"Runs in: {timer}")
+(fnode_counts, fnodes) = count_nodes()
 
+with open('{root_term}_links.json', 'w') as outfile:
+    json.dump(links, outfile)
+
+with open('{root_term}_nodes.json', 'w') as outfile:
+    json.dump(nodes, outfile)
+
+print(f"Number of nodes (pre-filter): {len(nodes)}")
+print(f"Number of nodes (post-filter): {len(fnode_counts)}")
+print(f"Runs in: {timer}")
