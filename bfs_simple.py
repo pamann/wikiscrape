696ec8fa8167e6cdf3b3e857328445a998205956
Pam <beardsell.pamela@gmail.com>
2020-11-19 12:40:08 -0500

    WIP: has threading of root and second tier

--- bfs_simple.py
+++ bfs_simple.py
@@ -7,6 +7,8 @@ from collections import Counter
 from concurrent.futures import ThreadPoolExecutor
 from concurrent import futures
 from multiprocessing import Pool
+import multiprocessing as mp
+from multiprocessing import Queue
 
 nodes = set()
 links = set()
@@ -21,7 +23,7 @@ def query_wiki(ttls):
     return query
 
 def process_comp_jobs(tt_page_s):
-    # print(tt_page_s)
+    print(tt_page_s)
     if 'links' in tt_page_s and 'linkshere' in tt_page_s:
         l = [v.title for v in tt_page_s.links]
         lh = [v.title for v in tt_page_s.linkshere]
@@ -30,7 +32,7 @@ def process_comp_jobs(tt_page_s):
         tt_bidi_links = lset.intersection(lhset)
         # print(f"Links: {len(l)}, lset: {len(lset)}")
         # print(f"Links here: {len(lh)}, lhset: {len(lhset)}")
-        print(f"Bidi links: {len(tt_bidi_links)}")
+        # print(f"Bidi links: {len(tt_bidi_links)}")
         aggregate_nodes(tt_bidi_links, 1)
         aggregate_links(tt_page_s.title, tt_bidi_links) 
 
@@ -52,19 +54,32 @@ def fetch_links(root_term):
             ilink_set = set(lh)
             bidi_links = [s.replace("'", "") for s in list(olink_set & ilink_set)]
             aggregate_links(root_term, bidi_links[0:50])
-            aggregate_nodes(bidi_links, 2)
-            with ThreadPoolExecutor(8) as executor:
+            aggregate_nodes(bidi_links, 2) # bidi links from root_term
+            with ThreadPoolExecutor(8) as executor: # start threaded bidi links of second tier
                 for bidi_link in bidi_links:
                     jobs.append(executor.submit(query_wiki, bidi_link))
-                for job in futures.as_completed(jobs):
-                    # Read result from future
-                    result = job.result()
-                    # print(result)
-                    if result:
-                        for tt_page in result:
-                            with ThreadPoolExecutor(8) as executor:
-                                for tt_page_s in tt_page.pages:
-                                    jobs.append(executor.submit(process_comp_jobs, tt_page_s))
+                # print(list(futures.as_completed(jobs)))
+                # queue = Queue(list(futures.as_completed(jobs)))
+
+                pool = Pool(processes=50)
+                [pool.apply_async(process_comp_jobs) # num_to_add is passed as an argument
+                    for res in futures.as_completed(jobs)]
+                pool.close()
+                pool.terminate()
+                pool.join()
+
+
+                # with Pool(5) as p:
+                #     p.map(process_comp_jobs, queue)
+                # for job in futures.as_completed(jobs):
+                #     # Read result from future
+                #     result = job.result()
+                #     # print(result)
+                #     if result:
+                #         for tt_page in result:
+                #             with ThreadPoolExecutor(8) as executor:
+                #                 for tt_page_s in tt_page.pages:
+                #                     jobs.append(executor.submit(process_comp_jobs, tt_page_s))
         else: 
             break
 
